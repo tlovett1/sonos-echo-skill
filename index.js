e2b8e7660ae939190c867e626e34a07331cd56a1
Taylor Lovett <tlovett88@gmail.com>
2017-01-02 01:54:29 -0500

    Continue

--- index.js
+++ index.js
@@ -1,91 +1,70 @@
-/**
- 
- Copyright 2016 Brian Donohue.
- 
-*/
-
 'use strict';
+
+var Sonos = require('sonos').Sonos
+var sonos = new Sonos(process.env.SONOS_HOST, process.env.SONOS_PORT)
 
 // Route the incoming request based on type (LaunchRequest, IntentRequest,
 // etc.) The JSON body of the request is provided in the event parameter.
-exports.handler = function (event, context) {
-    try {
-        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+exports.handler = function(event, context) {
+  try {
+    console.log('event.session.application.applicationId=' + event.session.application.applicationId);
 
-        /**
-         * Uncomment this if statement and populate with your skill's application ID to
-         * prevent someone else from configuring a skill that sends requests to this function.
-         */
-     
-     //if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.05aecccb3-1461-48fb-a008-822ddrt6b516") {
-      //   context.fail("Invalid Application ID");
-      //}
-
-        if (event.session.new) {
-            onSessionStarted({requestId: event.request.requestId}, event.session);
-        }
-
-        if (event.request.type === "LaunchRequest") {
-            onLaunch(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "IntentRequest") {
-            onIntent(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "SessionEndedRequest") {
-            onSessionEnded(event.request, event.session);
-            context.succeed();
-        }
-    } catch (e) {
-        context.fail("Exception: " + e);
+    if (event.session.new) {
+      onSessionStarted({requestId: event.request.requestId}, event.session);
     }
+
+    if (event.request.type === 'LaunchRequest') {
+      onLaunch(event.request, event.session, function callback(sessionAttributes, speechletResponse) {
+        context.succeed(buildResponse(sessionAttributes, speechletResponse));
+      });
+    } else if (event.request.type === 'IntentRequest') {
+      onIntent(event.request, event.session, function callback(sessionAttributes, speechletResponse) {
+        context.succeed(buildResponse(sessionAttributes, speechletResponse));
+      });
+    } else if (event.request.type === 'SessionEndedRequest') {
+      onSessionEnded(event.request, event.session);
+      context.succeed();
+    }
+  } catch (e) {
+    context.fail('Exception: ' + e);
+  }
 };
 
 /**
  * Called when the session starts.
  */
 function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
-        + ", sessionId=" + session.sessionId);
+  console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId + ", sessionId=" + session.sessionId);
 
-    // add any session init logic here
+  // add any session init logic here
 }
 
 /**
  * Called when the user invokes the skill without specifying what they want.
  */
 function onLaunch(launchRequest, session, callback) {
-    console.log("onLaunch requestId=" + launchRequest.requestId
-        + ", sessionId=" + session.sessionId);
+  console.log("onLaunch requestId=" + launchRequest.requestId + ", sessionId=" + session.sessionId);
 
-    var cardTitle = "Hello, World!"
-    var speechOutput = "You can tell Hello, World! to say Hello, World!"
-    callback(session.attributes,
-        buildSpeechletResponse(cardTitle, speechOutput, "", true));
+  var cardTitle = 'sonos';
+  var speechOutput = "You can tell sonos play tv, play song by title, play radio station, and stop."
+  callback(session.attributes, buildSpeechletResponse(cardTitle, speechOutput, "", true));
 }
 
 /**
  * Called when the user specifies an intent for this skill.
  */
 function onIntent(intentRequest, session, callback) {
-    console.log("onIntent requestId=" + intentRequest.requestId
-        + ", sessionId=" + session.sessionId);
+  console.log("onIntent requestId=" + intentRequest.requestId + ", sessionId=" + session.sessionId);
 
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
+  var intent = intentRequest.intent,
+  intentName = intentRequest.intent.name;
 
-    // dispatch custom intents to handlers here
-    if (intentName == 'TestIntent') {
-        handleTestRequest(intent, session, callback);
-    }
-    else {
-        throw "Invalid intent";
-    }
+  // dispatch custom intents to handlers here
+  if (intentName == 'sonos') {
+    handleTestRequest(intent, session, callback);
+  } else {
+    throw "Invalid intent";
+  }
 }
 
 /**
@@ -93,60 +72,58 @@ function onIntent(intentRequest, session, callback) {
  * Is not called when the skill returns shouldEndSession=true.
  */
 function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
-        + ", sessionId=" + session.sessionId);
+  console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId + ", sessionId=" + session.sessionId);
 
-    // Add any cleanup logic here
+  // Add any cleanup logic here
 }
 
 function handleTestRequest(intent, session, callback) {
-    callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Hello, World! robert", "", "true"));
+  callback(session.attributes, buildSpeechletResponseWithoutCard("Hello, World! robert", "", "true"));
 }
 
 // ------- Helper functions to build responses -------
 
 function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        card: {
-            type: "Simple",
-            title: title,
-            content: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
+  return {
+    outputSpeech: {
+      type: 'PlainText',
+      text: output
+    },
+    card: {
+      type: 'Simple',
+      title: title,
+      content: output
+    },
+    reprompt: {
+      outputSpeech: {
+        type: 'PlainText',
+        text: repromptText
+      }
+    },
+    shouldEndSession: shouldEndSession
+  };
 }
 
 function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
+  return {
+    outputSpeech: {
+      type: 'PlainText',
+      text: output
+    },
+    reprompt: {
+      outputSpeech: {
+        type: 'PlainText',
+        text: repromptText
+      }
+    },
+    shouldEndSession: shouldEndSession
+  };
 }
 
 function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
-    };
+  return {
+    version: '1.0',
+    sessionAttributes: sessionAttributes,
+    response: speechletResponse
+  };
 }
